2f3f9824268de333440a145bfc5eec2b2439ac42
Jay Mashauri <jaymash01@gmail.com>
2023-10-15 02:53:29 +0300

    Update sms service

--- app/Http/Services/SmsService.php
+++ app/Http/Services/SmsService.php
@@ -5,19 +5,17 @@ namespace App\Http\Services;
 use App\Models\Message;
 use App\Models\Patient;
 use App\Models\Preference;
-use Carbon\Carbon;
 use Exception;
 use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Log;
-use Psy\Util\Str;
 
 class SmsService
 {
-    private $sendMessageUrl = 'https://apisms.beem.africa/v1/send';
-    private $deliveryReportUrl = 'https://dlrapi.beem.africa/public/v1/delivery-reports';
-    private $smsApiKey = '776d2b75d12ab761';
-    private $smsSecretKey = 'MzM2N2JkZjYxMWJlNjM5NWZiZmRiZmM1ODZiOGNhMjA4ODVlZGNjMWVhMmUzMTZhZGYwMDBkZDUwNzc1MDdkZA==';
-    private $smsEncoding = 0;
+
+    private $secretKey = 't02t86j99hg4d174s0wdim6461d4xi9pj3828cfaft59lw07u0dzvh23gfyrl498';
+    private $sendMessageUrl = 'http://157.230.98.141:8010/sms/send';
+    private $deliveryReportUrl = 'http://157.230.98.141:8010/sms/delivery-report';
+    private $clientUrl = 'http://157.230.98.141:8010/sms/client';
 
     /**
      * Send message to patient.
@@ -37,10 +35,7 @@ class SmsService
         $phone = $patient->phone;
 
         if ($phone && str_starts_with($phone, '0') && strlen($phone) == 10) {
-            $recipients[] = [
-                'recipient_id' => Carbon::now()->getTimestamp(),
-                'dest_addr' => '255' . substr($patient->phone, 1),
-            ];
+            $recipients[] = '255' . substr($phone, 1);
         }
 
         if (!count($recipients)) {
@@ -55,17 +50,13 @@ class SmsService
         }
 
         $body = [
-            'source_addr' => $sender_name,
-            'encoding' => $this->smsEncoding,
-            'schedule_time' => '',
+            'senderName' => $sender_name,
             'message' => $message,
             'recipients' => $recipients,
         ];
 
-        $hash = base64_encode(sprintf('%s:%s', $this->smsApiKey, $this->smsSecretKey));
         $headers = [
-            'Authorization' => 'Basic ' . $hash,
-            'Content-Type' => 'application/json',
+            'x-access-token' => $this->secretKey,
         ];
 
         try {
@@ -75,15 +66,14 @@ class SmsService
             $response = $client->post($this->sendMessageUrl, $params)
                 ->getBody()
                 ->getContents();
+            $response = json_decode($response);
 
-            if ($response) {
-                Message::create([
-                    'message' => $message,
-                    'phone' => $patient->phone,
-                    'api_response' => $response,
-                    'patient_id' => $patient_id,
-                ]);
-            }
+            Message::create([
+                'message' => $message,
+                'phone' => $patient->phone,
+                'api_response' => json_encode($response->data),
+                'patient_id' => $patient_id,
+            ]);
 
             return $response;
         } catch (Exception $exception) {
@@ -94,31 +84,50 @@ class SmsService
 
     public function getMessageDeliveryReport($request_id, $recipient = null)
     {
-        $hash = base64_encode(sprintf('%s:%s', $this->smsApiKey, $this->smsSecretKey));
         $headers = [
-            'Authorization' => 'Basic ' . $hash,
-            'Content-Type' => 'application/json',
+            'x-access-token' => $this->secretKey,
         ];
 
         try {
-
             $client = new Client();
             $params['headers'] = $headers;
 
-            $url = sprintf('%s?request_id=%s', $this->deliveryReportUrl, $request_id);
+            $url = sprintf('%s?requestId=%s', $this->deliveryReportUrl, $request_id);
 
             if ($recipient) {
-                $url = sprintf('%s?dest_addr=%s&request_id=%s',
-                    $this->deliveryReportUrl,
-                    '255' . substr($recipient, 1),
-                    $request_id);
+                if (str_starts_with($recipient, '0')) {
+                    $recipient = '255' . substr($recipient, 1);
+                }
+
+                $url = sprintf('%s?recipient=%s&requestId=%s', $this->deliveryReportUrl, $recipient, $request_id);
             }
 
             $response = $client->get($url, $params)
                 ->getBody()
                 ->getContents();
             $response = json_decode($response);
-            return $response;
+            return $response->data;
+        } catch (Exception $exception) {
+            Log::debug($exception);
+            return null;
+        }
+    }
+
+    public function getClient()
+    {
+        $headers = [
+            'x-access-token' => $this->secretKey,
+        ];
+
+        try {
+            $client = new Client();
+            $params['headers'] = $headers;
+
+            $response = $client->get($this->clientUrl, $params)
+                ->getBody()
+                ->getContents();
+            $response = json_decode($response);
+            return $response->data;
         } catch (Exception $exception) {
             Log::debug($exception);
             return null;
